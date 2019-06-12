1a2b0804baf9f80487c68d60d191793aaaaaf27a
Maxim Yefremov <maxYefr@gmail.com>
2019-06-12 07:56:14 +0300

    using select case

--- nil_channel/main.go
+++ nil_channel/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"sync"
+	"math/rand"
+	"time"
 )
 
 // https://youtu.be/t9bEg2A4jsw?t=54
@@ -14,7 +14,7 @@ func main() {
 	b := asChan(2, 4, 6, 8)
 	c := merge(a, b)
 	for v := range c {
-		fmt.Println(v)
+		log.Println(v)
 	}
 }
 
@@ -23,31 +23,48 @@ func asChan(vs ...int) <-chan int {
 	go func() {
 		for _, v := range vs {
 			c <- v
+			time.Sleep(time.Microsecond * time.Duration(rand.Intn(1000)))
 		}
 		close(c)
 	}()
 	return c
 }
 
-func merge(a, b <-chan int) chan int {
+func merge(a, b <-chan int) <-chan int {
 	var result = make(chan int)
-	wg := sync.WaitGroup{}
-	wg.Add(2)
 	go func() {
-		defer wg.Done()
-		for v := range a {
-			result <- v
+		var aClosed, bClosed, resultClosed bool
+		for {
+			select {
+			case v, ok := <-a:
+				if !ok {
+					// log.Printf("a ok: %+v, aClosed: %+v, bClosed: %+v\n", ok, aClosed, bClosed)
+					aClosed = true
+					if aClosed && bClosed && !resultClosed {
+						log.Println("closing result")
+						close(result)
+						resultClosed = true
+						break
+					}
+					continue
+				}
+				result <- v
+			case v, ok := <-b:
+				if !ok {
+					// log.Printf("a ok: %+v, aClosed: %+v, bClosed: %+v\n", ok, aClosed, bClosed)
+					bClosed = true
+					if aClosed && bClosed && !resultClosed {
+						log.Println("closing result")
+						close(result)
+						resultClosed = true
+						break
+					}
+					continue
+				}
+				result <- v
+			}
 		}
-	}()
-	go func() {
-		defer wg.Done()
-		for v := range b {
-			result <- v
-		}
-	}()
-	go func() {
-		wg.Wait()
-		close(result)
+
 	}()
 	return result
 }
